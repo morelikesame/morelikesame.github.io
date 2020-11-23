d6a79dd989a961238a9d8a91a6e7da7416315dee
Sami <35745842+justsame@users.noreply.github.com>
2020-11-23 20:27:17 +0200

    NEW SITE
    ADDED META
    ADDED GOOGLE ANALYTICS
    NEW LAYOUT
    ONLY ONE PAGE
    MODALS
    SLANTS
    NEW ICON
    UPDATED LINKS

---
+++ js/js.js
@@ -0,0 +1,9 @@
+//Download modal activator
+$('#dlModal').on('shown.bs.modal', function () {
+    $('#dlModal').trigger('focus')
+  })
+
+//Info modal activator
+$('#infoModal').on('shown.bs.modal', function () {
+  $('#infoModal').trigger('focus')
+})
