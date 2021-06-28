2ef81a7d4b75a3da881948b1ab0cb6259c0beee4
Sami <35745842+justsame@users.noreply.github.com>
2021-06-28 22:02:27 +0300

    added alex quotes
    was bored, decided to do this, and now its here

---
+++ alexquotes/js/nicecock.js
@@ -0,0 +1,35 @@
+let bottomtext = document.getElementById('bottomtext');
+
+window.onload = (event) => {
+    bottomtext.innerHTML = btmTextList[Math.floor(Math.random()*btmTextList.length)].toUpperCase();
+  };
+
+let btmTextList = [
+    'cock',
+    'epic',
+    'nice',
+    'subscribe to rowwuk',
+    'ads bro',
+    'reference',
+    'maccas',
+    'rng',
+    'game',
+    'microsoft office 2007 free crack',
+    'gta san andreas no cd patch',
+    'and comedic',
+    '1999 reference',
+    'flintstone gummies',
+    'ricin poisoning',
+    'yiik alex',
+    'subway sandwich',
+    'airfryer',
+    'dream',
+    'ok',
+    'its called we do a little trolling',
+    'nuts',
+    'balls',
+    'earthbound reference',
+    'ayo the pizza here',
+    'something',
+    'epic'
+]
